b37fbb3b82fe8798d0915d3fb26255f9a6b9fcb1
unknown <shreyashmatele274@gmail.com>
2024-07-18 19:47:07 +0530

    Updated search functionality in explorer page.

--- src/Components/Explorer/Explorer.tsx
+++ src/Components/Explorer/Explorer.tsx
@@ -9,7 +9,16 @@ import copytext from "../../assets/copyText2.svg";
 import { getListTransactions } from "../../Config/API/api";
 import { ImageMapType2, TxObjectType } from "../../Config/types";
 import { formatEther } from "viem";
-import arrowLeft from '../../assets/arrow-left.svg'
+import arrowLeft from "../../assets/arrow-left.svg";
+import {
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spinner,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 type Props = {};
 
@@ -20,17 +29,22 @@ const imageUrl: ImageMapType2 = {
 
 type TxObjectArrayType = TxObjectType[];
 const Explorer = (props: Props) => {
-  const [transactions, settransactions] = useState<TxObjectArrayType | null>(null);
-  const [pageNo, setpageNo] = useState(1)
-  const [btn1disabled, setbtn1disabled] = useState(false)
-  const [btn2disabled, setbtn2disabled] = useState(false)
-  const [totalPages, settotalPages] = useState(1)
+  const [initailTxns, setInitailTxns] = useState<TxObjectArrayType | null>(null)
+  const [transactions, settransactions] = useState<TxObjectArrayType | null>(
+    null
+  );
+  const [pageNo, setpageNo] = useState(1);
+  const [btn1disabled, setbtn1disabled] = useState(false);
+  const [btn2disabled, setbtn2disabled] = useState(false);
+  const [totalPages, settotalPages] = useState(1);
+  const [inputAddress, setinputAddress] = useState("")
 
-  const getData = async (pageNo:number) => {
+  const getData = async (pageNo: number) => {
     const data = await getListTransactions(pageNo);
     console.log("tnxobj", data);
     settransactions(data.results);
-    settotalPages(data.totalPages)
+    setInitailTxns(data.results)
+    settotalPages(data.totalPages);
   };
 
   const shortenAddress = (
@@ -47,21 +61,20 @@ const Explorer = (props: Props) => {
     return "";
   };
 
-  const onClickPrev = () =>{
-    if(pageNo > 1){
-        setpageNo(pageNo-1)
-    }else{
-        setpageNo(totalPages)
+  const onClickPrev = () => {
+    if (pageNo > 1) {
+      setpageNo(pageNo - 1);
+    } else {
+      setpageNo(totalPages);
     }
-  }
-  const onClickNext = () =>{
-    if(pageNo > totalPages){
-        setpageNo(1)
-    }else{
-        setpageNo(pageNo+1)
+  };
+  const onClickNext = () => {
+    if (pageNo > totalPages) {
+      setpageNo(1);
+    } else {
+      setpageNo(pageNo + 1);
     }
-    
-  }
+  };
 
   const formatToken = (numStr: string) => {
     if (parseFloat(numStr)) {
@@ -76,9 +89,37 @@ const Explorer = (props: Props) => {
     return numStr;
   };
 
+  function filterObjectsByAddress(objects:TxObjectArrayType, address:string) {
+    return objects.filter(obj => {
+        const addressLowerCase = address.toLowerCase();
+        return (
+            (obj.inputTxHash && obj.inputTxHash.toLowerCase().includes(addressLowerCase)) ||
+            (obj.outputTxHash && obj.outputTxHash.toLowerCase().includes(addressLowerCase)) ||
+            (obj.inputAddress && obj.inputAddress.toLowerCase().includes(addressLowerCase)) ||
+            (obj.outputAddress && obj.outputAddress.toLowerCase().includes(addressLowerCase))
+        );
+    });
+}
+
+
+
   useEffect(() => {
     getData(pageNo);
   }, [pageNo]);
+
+  useEffect(() => {
+    if(transactions){
+      if(inputAddress != ""){
+      const filteredObjects = filterObjectsByAddress(transactions, inputAddress);
+      settransactions(filteredObjects)
+      }
+      else{
+        settransactions(initailTxns)
+      }
+    }
+    
+  }, [inputAddress])
+  
 
   return (
     <div className="ExplorerApp">
@@ -92,6 +133,75 @@ const Explorer = (props: Props) => {
             </div>
         </div>
         </div> */}
+      <div className="filter-container dflex-row">
+        <div className="input-search">
+          <img src={SearchIcon} />
+          <input
+            type="text"
+            placeholder="Search by transaction hash or address"
+            value={inputAddress}
+            onChange={(e) => setinputAddress(e.target.value)}
+          />
+        </div>
+        <div className="filters dflex-row">
+          <div className="filter-chain1 dflex-row">
+            Filter
+            <Menu>
+              <MenuButton
+                as={Button}
+                sx={{
+                  backgroundColor: "#fff",
+                  borderRadius: "24px",
+                  fontFamily: "Inter",
+                  fontSize: "16px",
+                  fontWeight: "400",
+                  lineHeight: "20px",
+                  textAlign: "left",
+                  color:"#878794"
+                }}
+                rightIcon={<ChevronDownIcon />}
+              >
+                Actions
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Download</MenuItem>
+                <MenuItem>Create a Copy</MenuItem>
+                <MenuItem>Mark as Draft</MenuItem>
+                <MenuItem>Delete</MenuItem>
+                <MenuItem>Attend a Workshop</MenuItem>
+              </MenuList>
+            </Menu>
+          </div>
+          <div className="filter-chain1 dflex-row">
+            To
+            <Menu>
+              <MenuButton 
+                as={Button} 
+                rightIcon={<ChevronDownIcon />}
+                sx={{
+                  backgroundColor: "#fff",
+                  borderRadius: "24px",
+                  fontFamily: "Inter",
+                  fontSize: "16px",
+                  fontWeight: "400",
+                  lineHeight: "20px",
+                  textAlign: "left",
+                  color:"#878794"
+                }}
+              >
+                Actions
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Download</MenuItem>
+                <MenuItem>Create a Copy</MenuItem>
+                <MenuItem>Mark as Draft</MenuItem>
+                <MenuItem>Delete</MenuItem>
+                <MenuItem>Attend a Workshop</MenuItem>
+              </MenuList>
+            </Menu>
+          </div>
+        </div>
+      </div>
       <div className="table-container">
         <table>
           <thead>
@@ -105,7 +215,7 @@ const Explorer = (props: Props) => {
             </tr>
           </thead>
           <tbody>
-            {transactions !== null &&
+            {transactions !== null ? (
               transactions.map((item, index) => (
                 <tr>
                   <td>
@@ -147,19 +257,37 @@ const Explorer = (props: Props) => {
                   </td>
                   <td>{item.updatedAt}</td>
                 </tr>
-              ))}
-
+              ))
+            ) : (
               <tr>
                 <td colSpan={6}>
-                    <div className="table-footer">
-                    <button className="previous btns" onClick={onClickPrev} disabled={pageNo < 1}><img src={arrowLeft} className="prev-arrow" />Previous</button>
-                    <div className="pagination">
-
-                    </div>
-                    <button className="next btns" onClick={onClickNext} disabled={pageNo > totalPages}>Next <img src={arrowLeft} className="next-arrow"/></button>
-                    </div>
+                  <Spinner size="md" />
                 </td>
               </tr>
+            )}
+
+            <tr>
+              <td colSpan={6}>
+                <div className="table-footer">
+                  <button
+                    className="previous btns"
+                    onClick={onClickPrev}
+                    disabled={pageNo < 1}
+                  >
+                    <img src={arrowLeft} className="prev-arrow" />
+                    Previous
+                  </button>
+                  <div className="pagination"></div>
+                  <button
+                    className="next btns"
+                    onClick={onClickNext}
+                    disabled={pageNo > totalPages}
+                  >
+                    Next <img src={arrowLeft} className="next-arrow" />
+                  </button>
+                </div>
+              </td>
+            </tr>
           </tbody>
         </table>
       </div>
