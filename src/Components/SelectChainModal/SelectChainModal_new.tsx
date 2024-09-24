ecd923aeaf63efd08388ca1c74028e3566b962d6
unknown <shreyashmatele274@gmail.com>
2024-09-24 15:19:18 +0530

    removed morph restriction.

--- src/Components/SelectChainModal/SelectChainModal_new.tsx
+++ src/Components/SelectChainModal/SelectChainModal_new.tsx
@@ -73,9 +73,9 @@ const SelectChainModalNew = observer(({open,isOpen,onOpen, onClose,setModal,chai
             {chainList.map((chain)=>{
                     var key = chain.name
                     if(chain.id === 1) return ""
-                    if(toselectChain === 1 && chain.id === 2810){
-                      return ""
-                    }
+                    // if(toselectChain === 1 && chain.id === 2810){
+                    //   return ""
+                    // }
                     if(toselectChain === 2 && chain.id === chain_1.id){
                         return("")
                     }
