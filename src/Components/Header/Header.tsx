0547da207cf69ff614a43e651bb4f41d324f77bd
unknown <shreyashmatele274@gmail.com>
2024-11-09 23:32:39 +0530

    notification header changed.

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -7,11 +7,12 @@ type Props = {}
 
 const Header = (props: Props) => {
   const [selected, setselected] = useState("")
-  const [closeNotification, setcloseNotification] = useState(true)
+  const [closeNotification, setcloseNotification] = useState(false)
   return (
     <div className="nav-root">
       <div className={`nav-notification ${closeNotification ? "hideDiv":""}`}>
-        🚧 Website Maintenance in Progress 🚧    We're currently performing some updates and will be back online shortly. Thank you for your patience!
+        Bridging services are temporarily paused as we transition to GasYard V2.
+        {/* 🚧 Website Maintenance in Progress 🚧    We're currently performing some updates and will be back online shortly. Thank you for your patience! */}
         {/* Welcome Gasyard testnet. For this phase we’ve limited the Bridge amount to 0.05 ETH max on all networks! */}
         {/* New Quest Live! Complete tasks like bridging ETH and providing liquidity on Kakarot testnet to earn rewards! */}
         {/* <a href="https://superboard.xyz/quests/gasyard-gas-orchestration-protocol" target="_blank" className="quest-btn">Go to Quest</a> */}
