5d9a070fea97d3e39156335baaedbeb14b46d619
unknown <shreyashmatele274@gmail.com>
2024-11-09 23:22:05 +0530

    notification header changed.

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -12,11 +12,12 @@ type Props = {}
 
 const Header = (props: Props) => {
   const [selected, setselected] = useState("")
-  const [closeNotification, setcloseNotification] = useState(true)
+  const [closeNotification, setcloseNotification] = useState(false)
   return (
     <div className="nav-root">
       <div className={`nav-notification ${closeNotification ? "hideDiv":""}`}>
-        🚧 Website Maintenance in Progress 🚧    We're currently performing some updates and will be back online shortly. Thank you for your patience!
+        Bridging services are temporarily paused as we transition to GasYard V2.
+        {/* 🚧 Website Maintenance in Progress 🚧    We're currently performing some updates and will be back online shortly. Thank you for your patience! */}
         {/* Welcome to Movement Testnet by Gasyard.fi. For this phase we’ve limited the Bridge amount to 0.05 ETH / $200 max on all networks! */}
         {/* New Quest Live! Complete tasks like bridging ETH and providing liquidity on Morph testnet to earn points! */}
         {/* <a href="https://app.galxe.com/quest/Gasyard" target="_blank" className="quest-btn">Go to Quest</a> */}
